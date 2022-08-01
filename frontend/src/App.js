0b5b392345b915f27429a142c0a261dc8e44fa22
susanna1221 <susanna1221@naver.com>
2022-08-01 13:42:19 +0900

    feat: add floating logo
    - 움직이는 로고 완성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import styled, { keyframes } from 'styled-components';
+import BookImg from 'images/book-img.svg';
+import NoteImg from 'images/note-img.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={BookImg} alt="" />
+      <Note src={NoteImg} alt="" />
     </div>
   );
 }
+
+const noteFloating = keyframes`
+0% {
+  opacity: 1;
+  bottom: 250px
+}
+50% {
+  opacity: 0.2;
+  bottom: 300px;
+}
+100% {
+  opacity: 1;
+  bottom: 250px;
+}
+`;
+
+const Note = styled.img`
+  position: relative;
+  bottom: 250px;
+  right: 150px;
+  animation: ${noteFloating} 1.5s 0s Infinite linear none;
+`;
 
 export default App;
