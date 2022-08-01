6dddd651904b2157df79b96a6bdebf6f8bfaeca2
susanna1221 <susanna1221@naver.com>
2022-08-01 17:54:03 +0900

    feat: music player
    - 음악 플레이어 개발 중
    - 음악 여러개 가능하도록 조정 중

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Login from 'pages/loginPage';
 import { Route, Routes } from 'react-router-dom';
 import MainPage from 'pages/mainPage';
+import MusicPlayer from 'components/MusicPlayer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/mainPage" element={<MainPage />} />
+        <Route path="/music" element={<MusicPlayer />} />
       </Routes>
     </div>
   );
--- frontend/src/components/MusicPlayer.jsx
+++ frontend/src/components/MusicPlayer.jsx
@@ -3,30 +3,84 @@ import styled from 'styled-components';
 
 function MusicPlayer() {
   const audioPlayer = useRef();
-  const [music, setMusic] = useState(
-    'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3',
-  );
+  // eslint-disable-next-line operator-linebreak
+  const music = [
+    {
+      title: 'I owe you',
+      link: 'https://ia903004.us.archive.org/14/items/78rpmCollection1920s1930sPopularMusic/001%20I%20Owe%20You.mp3',
+    },
+    {
+      title: 'Sh-Boom',
+      link: 'https://ia801800.us.archive.org/10/items/78_sh-boom_the-chords-keyes-feaster-mcrae-edwards_gbia0267115a/SH-BOOM%20-%20THE%20CHORDS%20-%20Keyes%20-%20Feaster%20-%20McRae.mp3',
+    },
+    {
+      title: 'You talk just like me',
+      link: 'https://ia801703.us.archive.org/22/items/78_dats-love-act-1-scene-1_carmen-jones-orchestra-and-carmen-jones-chorus-georg_gbia8001722/03%20-%20YOU%20TALK%20JUST%20LIKE%20MY%20-%20CARLOTTA%20FRANZELL%20and%20LUTHER%20SAXON.mp3',
+    },
+  ];
+  const [musicIndex, setMusicIndex] = useState(0);
   const [currentTime, setCurrentTime] = useState(0);
   const [seekValue, setSeekValue] = useState(0);
   const play = () => {
     audioPlayer.current.play();
   };
   const pause = () => {
-    audioPlayer.current.play();
+    audioPlayer.current.pause();
   };
-  const stop = () => {
-    audioPlayer.current.stop();
+  const nextSong = () => {
+    setMusicIndex((musicIndex + 1) % music.length);
+  };
+  const prevSong = () => {
+    setMusicIndex((musicIndex - 1 + music.length) % music.length);
   };
   const onPlaying = () => {
     setCurrentTime(audioPlayer.current.currentTime);
-    seekValue(
+    setSeekValue(
       (audioPlayer.current.currentTime / audioPlayer.current.duration) * 100,
     );
   };
 
   return (
     <MusicPlayerWrapper>
-      <audio src={music} ref={audioPlayer} onTimeUpdate={onPlaying} />
+      <audio
+        src={music[musicIndex].link}
+        ref={audioPlayer}
+        onTimeUpdate={onPlaying}
+      >
+        <track default kind="captions" />
+      </audio>
+      <br />
+      <p>
+        {`${parseInt(currentTime / 60, 10)}:${String(
+          parseInt(currentTime % 60, 10),
+        ).padStart(2, '0')}`}
+      </p>
+      <input
+        type="range"
+        min="0"
+        max="100"
+        step="1"
+        value={seekValue}
+        onChange={(e) => {
+          const seekto = audioPlayer.current.duration * (+e.target.value / 100);
+          audioPlayer.current.currentTime = seekto;
+          setSeekValue(e.target.value);
+        }}
+      />
+      <div>
+        <button type="button" onClick={play}>
+          play
+        </button>
+        <button type="button" onClick={pause}>
+          pause
+        </button>
+        <button type="button" onClick={nextSong}>
+          next
+        </button>
+        <button type="button" onClick={prevSong}>
+          prev
+        </button>
+      </div>
     </MusicPlayerWrapper>
   );
 }
