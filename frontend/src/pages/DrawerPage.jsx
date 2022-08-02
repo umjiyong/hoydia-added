9bf09ee8304f16f31c7a1da2b972eb9b47f6677e
Hyeon Yeol Kim <gusduf612@gmail.com>
2022-08-02 14:47:26 +0900

    feat : create diary drawerpage
    diary drawerpage 초안 완성 추후에 DB에 데이터 추가해서 무한 스크롤 테스트해 볼 예정

--- frontend/src/pages/DrawerPage.jsx
+++ frontend/src/pages/DrawerPage.jsx
@@ -1,21 +1,103 @@
-import React from 'react';
 import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import ReactLoading from 'react-loading';
+import diary from 'assets/diary.png';
+import diarytable from 'assets/diaryTable.png';
 import Navbar from 'components/Navbar';
 
-const Content = styled.div`
+const LoaderWrap = styled.div`
+  width: 100%;
+  height: 80%;
   display: flex;
-  flex-direction: column;
-  gap: 15px;
-  margin-top: 30px;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+`;
+const DrawerContainer = styled.div``;
+
+const DiaryContainer = styled.div`
+  display: flex;
+  justify-content: center;
 `;
 
-function drawerPage() {
+const Colcontainer = styled.div`
+  flex: ${(props) => props.size};
+`;
+const Diary = styled.img`
+  max-width: 240px;
+  max-height: 300px;
+  // margin: 40px;
+`;
+
+const DiaryTable = styled.img`
+  display: flex;
+  margin-bottom: 10px;
+  width: 100%;
+  height: 178.3px;
+`;
+
+function DrawerPage() {
+  const itemList = [1, 2, 3, 4, 5, 6]; // ItemList
+  const [target, setTarget] = useState(''); // target
+  const [isLoding, setIsLoding] = useState(false); // isloding
+
+  const onIntersect = async ([entry], observer) => {
+    if (entry.isIntersecting && !isLoding) {
+      observer.unobserve(entry.target);
+      setIsLoding(true);
+      // 데이터를 가져오는 부분
+      setIsLoding(false);
+      observer.observe(entry.target);
+    }
+  };
+
+  useEffect(() => {
+    let observer;
+    if (target) {
+      // callback 함수, option
+      observer = new IntersectionObserver(onIntersect, {
+        threshold: 0.4,
+      });
+      observer.observe(target); // 타겟 엘리먼트 지정
+    }
+    return () => observer && observer.disconnect();
+  }, [target]);
+
   return (
-    <div className="drawer">
+    <div className="App">
       <Navbar />
-      <Content> 내용 </Content>
+      {itemList.map((item) => (
+        <div className="Item" key={item}>
+          <DrawerContainer>
+            <DiaryContainer>
+              <Colcontainer size={1}> </Colcontainer>
+              <Colcontainer size={2}>
+                <Diary src={diary} alt="diary" />
+              </Colcontainer>
+              <Colcontainer size={1}> </Colcontainer>
+              <Colcontainer size={2}>
+                <Diary src={diary} alt="diary" />
+              </Colcontainer>
+              <Colcontainer size={1}> </Colcontainer>
+              <Colcontainer size={2}>
+                <Diary src={diary} alt="diary" />
+              </Colcontainer>
+              <Colcontainer size={1}> </Colcontainer>
+            </DiaryContainer>
+            <DiaryTable src={diarytable} alt="diarytable" />
+          </DrawerContainer>
+        </div>
+      ))}
+      {isLoding ? (
+        <LoaderWrap>
+          <ReactLoading type="spin" color="#A593E0" />
+        </LoaderWrap>
+      ) : (
+        ''
+      )}
+      <div ref={setTarget}> </div>
     </div>
   );
 }
 
-export default drawerPage;
+export default DrawerPage;
